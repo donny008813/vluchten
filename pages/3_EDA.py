37e271016000e3637d3d3c6f0393496816d56c27
donny008813 <167076782+donny008813@users.noreply.github.com>
2024-10-10 13:20:52 +0200

    Update 3_EDA.py

--- pages/3_EDA.py
+++ pages/3_EDA.py
@@ -47,10 +47,13 @@ vluchten_copy['maand'] = vluchten_copy['STD'].dt.month
 
 vluchten_vertraagd = vluchten_copy[vluchten_copy['vertraagd'] == 1]
 
+st.title('EDA')
+
 # Maak de eerste plot
 fig1, ax1 = plt.subplots()
 
-# Plot de totale populatie
+# Plot de vertragingen per dag
+plt.figure(figsize=(10, 6))
 sns.countplot(data=vluchten_copy, x='dag', hue='vertraagd')
 
 # Labels en titel
@@ -61,3 +64,15 @@ ax1.set_xlabel('Day')
 
 # Plot de eerste plot
 st.pyplot(fig1)
+
+# Maak de tweede plot
+fig2, ax2 = plt.subplots()
+
+# Plot de vertragingen per maand
+plt.figure(figsize=(10,6))
+sns.countplot(data=vluchten_copy, x='maand', hue='vertraagd')
+
+# Labels en titel
+ax2.set_title('Count of Delays and Non-Delays per Month')
+ax2.set_ylabel('Count')
+ax2.set_xlabel('Maand')
