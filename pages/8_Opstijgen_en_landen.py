fe26245e57f7c64567ee425ff31572efb62300e0
JelleVerhaaf <jelle.verhaaf@hva.nl>
2024-11-06 12:11:12 +0100

    Update 8_Opstijgen_en_landen.py

--- pages/8_Opstijgen_en_landen.py
+++ pages/8_Opstijgen_en_landen.py
@@ -5,18 +5,17 @@ import pandas as pd
 from datetime import datetime
 import seaborn as sns
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-
 
 st.set_page_config(page_title="Vergelijking van opstijgen en landen")
 
 st.markdown("# Opstijgen en landen")
 st.sidebar.header("Opstijgen en landen")
 st.write(
-    """In deze grafiek zullen van verschillende vluchten het opsteigen en landen getoond worden.
+    """In deze grafiek zullen van verschillende vluchten het opstijgen en landen getoond worden.
     Hierin is voor elke vlucht de hoogte en de tijd te zien."""
 )
 
+# Load the data
 flight_1_30 = pd.read_excel('https://raw.githubusercontent.com/donny008813/vluchten/main/30Flight1.xlsx')
 flight_2_30 = pd.read_excel('https://raw.githubusercontent.com/donny008813/vluchten/main/30Flight2.xlsx')
 flight_3_30 = pd.read_excel('https://raw.githubusercontent.com/donny008813/vluchten/main/30Flight3.xlsx')
@@ -25,6 +24,7 @@ flight_5_30 = pd.read_excel('https://raw.githubusercontent.com/donny008813/vluch
 flight_6_30 = pd.read_excel('https://raw.githubusercontent.com/donny008813/vluchten/main/30Flight6.xlsx')
 flight_7_30 = pd.read_excel('https://raw.githubusercontent.com/donny008813/vluchten/main/30Flight7.xlsx')
 
+# Filter data
 flight_1_30 = flight_1_30[flight_1_30['[3d Altitude Ft]'] >= 500]
 y1_30 = flight_1_30['[3d Altitude Ft]']
 flight_2_30 = flight_2_30[flight_2_30['[3d Altitude Ft]'] >= 500]
@@ -40,14 +40,16 @@ y6_30 = flight_6_30['[3d Altitude Ft]']
 flight_7_30 = flight_7_30[flight_7_30['[3d Altitude Ft]'] >= 500]
 y7_30 = flight_7_30['[3d Altitude Ft]']
 
-x_1tijd = flight_1_30['Time (secs)']
-x_2tijd = flight_2_30['Time (secs)']
-x_3tijd = flight_3_30['Time (secs)']
-x_4tijd = flight_4_30['Time (secs)']
-x_5tijd = flight_5_30['Time (secs)']
-x_6tijd = flight_6_30['Time (secs)']
-x_7tijd = flight_7_30['Time (secs)']
+# Convert time from seconds to minutes
+x_1tijd = flight_1_30['Time (secs)'] / 60
+x_2tijd = flight_2_30['Time (secs)'] / 60
+x_3tijd = flight_3_30['Time (secs)'] / 60
+x_4tijd = flight_4_30['Time (secs)'] / 60
+x_5tijd = flight_5_30['Time (secs)'] / 60
+x_6tijd = flight_6_30['Time (secs)'] / 60
+x_7tijd = flight_7_30['Time (secs)'] / 60
 
+# Plotting
 fig1, ax1 = plt.subplots()
 sns.lineplot(data=flight_1_30, x=x_1tijd, y=y1_30, label='vlucht 1')
 sns.lineplot(data=flight_2_30, x=x_2tijd, y=y2_30, label='vlucht 2')
@@ -57,7 +59,8 @@ sns.lineplot(data=flight_5_30, x=x_5tijd, y=y5_30, label='vlucht 5')
 sns.lineplot(data=flight_6_30, x=x_6tijd, y=y6_30, label='vlucht 6')
 sns.lineplot(data=flight_7_30, x=x_7tijd, y=y7_30, label='vlucht 7')
 plt.legend()
-ax1.set_xlabel('Tijd in seconde')
-ax1.set_ylabel('Altitude in feets')
-ax1.set_title('Altitude tegenover tijd in seconde van de 7 vluchten')
+ax1.set_xlabel('Tijd in minuten')  # Updated label
+ax1.set_ylabel('Altitude in feet')
+ax1.set_title('Altitude tegenover tijd in minuten van de 7 vluchten')
 st.pyplot(fig1)
+
