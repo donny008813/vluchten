042c12ec486a9f24adafd5d69527e1514f5a5a73
donny008813 <167076782+donny008813@users.noreply.github.com>
2024-11-06 13:40:09 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ import pandas as pd
 from streamlit_folium import st_folium
 
 st.set_page_config(
-    page_title="Hallo en welkom bij de vliegveldenmap"
+    page_title="Hallo en welkom bij de verbetering van case 3"
 )
 
 st.write("# Welkom bij het dashboard van de verbeteringen van case 3.")
@@ -19,6 +19,18 @@ Eerst zal er gekeken worden naar de kaart die voor deze opdracht is opgesteld en
 naar het aantal vluchten en hoe wij deze visualisaties hebben verbeterd. Ook nog een kleine verbetering in de visualisatie voor het vergelijken van het opstijgen en landen van 
 verschillende vluchten. En ten slotte wordt het oude model getoond en hoe wij deze mogelijk hebben verbeterd door extra features te maken en te kijken of deze invloed hebben. Met 
 de nieuwe scores van het model worden samen getoond met die van het oude model.
+
+Bronnen die zijn gebruikt:
+
+- Data beschikbaar gesteld door de HvA: https://dlo.mijnhva.nl/d2l/le/content/614352/viewContent/2210753/View
+
+- Data beschikbaar gesteld door de HvA: https://dlo.mijnhva.nl/d2l/le/content/614352/viewContent/2210755/View
+
+- Lessen van Datacamp
+
+- Hoor en werkcolleges van Minor Data Science Introduction to Data Science
+
+- ChatGPT
 """
 )
 
